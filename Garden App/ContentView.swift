5e1a3a220cabcce38874cdfcc17ecdec654a00f5
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2022-11-29 09:25:17 -0600

    Initial Commit

---
+++ Garden App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Garden App
+//
+//  Created by caden christesen on 11/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Garden App/Garden_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Garden_AppApp.swift
+//  Garden App
+//
+//  Created by caden christesen on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Garden_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
