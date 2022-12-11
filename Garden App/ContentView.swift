c77ca9f83c8dbb8032a462bc6ca07bfbca627b3c
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2022-12-11 14:57:55 -0600

    extended app color to full screen

--- Garden App/ContentView.swift
+++ Garden App/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 Spacer()
             }
             Spacer()
-        }.background(Color.mint)
+        }.background(Color.mint.edgesIgnoringSafeArea(.all))
         
     }
     
